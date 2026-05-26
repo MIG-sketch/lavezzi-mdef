2d0ed261e1e63944ec79a5460e91590cf4bea124
MIG-sketch <francesco.mignogna@students.iaac.net>
2026-05-26 12:58:10 +0200

    Update menu.js

--- docs/javascrips/menu.js
+++ docs/javascrips/menu.js
@@ -1,1 +1,19 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".md-header");
+
+  if (!header) return;
+
+  const menu = document.createElement("div");
+  menu.className = "custom-top-menu";
+
+  menu.innerHTML = `
+    <a href="/lavezzi-mdef/">Home</a>
+    <a href="/lavezzi-mdef/about/me/">About</a>
+    <a href="/lavezzi-mdef/term1/01-Design%20studio%2001/">Term 1</a>
+    <a href="/lavezzi-mdef/term2/Extended%20Intelligences/">Term 2</a>
+    <a href="/lavezzi-mdef/term3/Cognitive%20Orgies%20III/">Term 3</a>
+  `;
+
+  header.insertAdjacentElement("afterend", menu);
+});
